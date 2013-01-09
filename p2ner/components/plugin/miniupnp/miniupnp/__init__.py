f151d7f72d5681c4d0c28b38d0647cd1af045e6f
sakis <sakis@f74ccb00-3321-43bf-ac2d-2a3b9facab8b>
2013-01-09 10:41:22 +0000

    miniupnpc
    git-svn-id: svn://nam.ece.upatras.gr/p2ner/trunk/p2ner@470 f74ccb00-3321-43bf-ac2d-2a3b9facab8b

---
+++ p2ner/components/plugin/miniupnp/miniupnp/__init__.py
@@ -0,0 +1,1 @@
+from miniupnp import miniUPNP
---
+++ p2ner/components/plugin/miniupnp/miniupnp/miniupnp.py
@@ -0,0 +1,77 @@
+#   Copyright 2012 Loris Corazza, Sakis Christakidis
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from twisted.internet import reactor,defer
+from miniupnpc import UPnP
+from p2ner.util.utilities import findNextUDPPort
+
+
+class miniUPNP(object):
+    def __init__(self,cport,dport,log=None):
+        self.cport=cport
+        self.dport=dport
+        self.log=log
+        
+    
+    def start(self):
+        self.upnp=UPnP()
+        self.upnp.discoverdelay=3000
+        devices=self.upnp.discover()
+        if not devices:
+            raise ValueError('no devices found')
+        f=self.upnp.selectigd()    
+        self.log.info('upnp device found')
+        self.ip=self.upnp.lanaddr
+        
+        newcport=self.addPortMapping(self.cport)
+        if newcport:
+            newdport=self.addPortMapping(self.dport)
+            if newdport:
+                return (newcport,newdport) 
+            else:
+                raise ValueError('could not forward control port')
+        else:
+            raise ValueError('could not forward data port')
+
+
+    def addPortMapping(self,port):
+        self.log.info('trying to forward port %d',port)
+        pm=self.upnp.getspecificportmapping(port,'UDP')
+        if pm:
+            if self.ip==pm[0]:
+                print 'port is already forwarded for this ip'
+                self.log.info('port %d is already forwarded for %s',port,self.ip)
+                return port
+            else:
+                return self.addPortMapping(port=findNextUDPPort(port))
+        try:    
+            b = self.upnp.addportmapping(port, 'UDP', self.ip, port, 'P2NER', '')
+        except:
+            self.log.warning('a problem occured trying to forward port %d',port)
+            self.log.warning('validating if port %d was correctly forwarded',port)
+            self.upnp.discover()
+            d=self.upnp.selectigd()
+            pm=self.upnp.getspecificportmapping(port,'UDP')
+            b=False
+            if pm and self.ip in pm:
+                b=True
+
+        if b:
+            self.log.info('port %d was successfully forwarded',port)
+            return port
+        else:
+            self.log.warning("couldn't forward port %d",port)
+            return False
+        
+
