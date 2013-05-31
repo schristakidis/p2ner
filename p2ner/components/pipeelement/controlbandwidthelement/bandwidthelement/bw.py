d03d046504e1e8e5822c13a4274e4618891186ab
loox <loox@f74ccb00-3321-43bf-ac2d-2a3b9facab8b>
2013-05-31 17:39:57 +0000

    new el
    git-svn-id: svn://nam.ece.upatras.gr/p2ner/trunk/p2ner@579 f74ccb00-3321-43bf-ac2d-2a3b9facab8b

---
+++ p2ner/components/pipeelement/controlbandwidthelement/bandwidthelement/bw.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+#   Copyright 2012 Loris Corazza, Sakis Christakidis
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from p2ner.abstract.pipeelement import PipeElement
+from twisted.internet import reactor
+from p2ner.base.Peer import Peer
+from collections import deque
+import time
+
+class BandwidthElement(PipeElement):
+
+    def initElement(self, bw=200000, thres=3):
+        self.log.info('BlockHeaderElement loaded')
+        self.bw = bw
+        self.que = deque()
+        self.stuck = True
+        self.thres = thres
+        self.bwSet=False
+        
+    
+    def send(self, res, msg, data, peer):
+        for r in res:
+            pack = (r, peer)
+            self.que.append(pack)
+        if self.stuck:
+            self.stuck = False
+            reactor.callLater(0, self.sendfromque)
+        self.breakCall()
+        return res
+    
+    def askdata(self):
+        self.forwardprev("produceblock").callback("")
+    
+    def sendfromque(self):
+        if len(self.que) == 0:
+            self.stuck = True
+            self.askdata()
+            return
+        res, peer = self.que.popleft()
+        if len(self.que) <= self.thres:
+            self.askdata()
+        #CHECK IF PEER BW IS SET
+        #bw = getattr(peer, "bw", self.bw)
+        bw=peer.bw
+        #if bw==0:
+        #    peer.bw=self.bw
+         #   bw=self.bw
+        #SET BW TO THE MIN
+        #bw = min(bw, self.bw)
+        if self.bwSet:
+            bw=self.bw
+            peer.bw=bw
+            
+        print bw,peer.bw
+        nextiter=1.0*len(res)/bw
+        #print 'next iter:',nextiter
+        reactor.callLater(nextiter, self.sendfromque)
+        #print 'next:',nextiter
+        #print 'total:',time.time()+nextiter
+        self.forwardnext("send", None, None, peer).callback(res)
+        
+    def setBW(self, d, bw):
+        self.bwSet=True
+        self.bw=bw
+        if self.bw>2*1024:
+            self.bw=2*1024
+            
+        self.bw =0.9 * self.bw*1024
+        print 'setting bw to ',self.bw
+        return self.bw
+
+
