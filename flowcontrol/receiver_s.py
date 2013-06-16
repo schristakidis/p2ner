8c4752e5d61d45ed25cd2408765ed0355e1df637
loox <loox@f74ccb00-3321-43bf-ac2d-2a3b9facab8b>
2013-06-16 17:55:29 +0000

    receiver updated
    git-svn-id: svn://nam.ece.upatras.gr/p2ner/trunk/p2ner@639 f74ccb00-3321-43bf-ac2d-2a3b9facab8b

--- flowcontrol/receiver_s.py
+++ flowcontrol/receiver_s.py
@@ -4,19 +4,28 @@ import threading
 import sys
 import xmlrpclib
 import struct
-#import time
+import csv
+import time
 #from cPickle import dumps
 
-IP = '150.140.187.171'
+IP = '192.168.1.2'
 PORT = 30000
 
-serverIP = '94.64.199.248'
+serverIP = '192.168.0.1'
 serverXport = 8000
 proxy = xmlrpclib.ServerProxy("http://%s:%d/" % (serverIP, serverXport), allow_none=True)
+cnt = 0
+err = 0
+clname = "client" + IP.split('.')[2]
+csvfile = open(clname+".csv", 'wb')
+writer = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+cLock = threading.RLock()
+START = threading.Event()
 
-class UDPreceiver(object):
+class UDPreceiver(threading.Thread):
     
     def __init__(self, port=30001, ackport=30000):
+        threading.Thread.__init__(self)
         self.socketUDPdata = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socketUDPack = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socketUDPdata.bind(("0.0.0.0", port))
@@ -24,15 +33,21 @@ class UDPreceiver(object):
         #self.receivedTime={}
         self.acked=0
         self.lastAcked=0
+        self.notrunning = True
         
     def run(self):
+        global cnt,err,START,cLock
         while True:
             data, addr = self.socketUDPdata.recvfrom(1500)
             #print len(data)
-            
+            cLock.acquire()
             try:
-                seq = str(struct.unpack("l", data[0:8])[0])
+                seq = str(struct.unpack("l", data[0:4])[0])
                 #self.receivedTime[seq]=time.time()
+                cnt += 1
+                if self.notrunning:
+                  START.set()
+                  self.notrunning = False
                 print 'received:',seq
             except:
                 print 'fail'
@@ -41,13 +56,34 @@ class UDPreceiver(object):
             
 	    if int(seq)!=self.lastAcked+1:
 		print 'missed packet'
+                err += int(seq)-self.lastAcked
 
+            cLock.release()
             if seq>self.lastAcked:
                 self.lastAcked=int(seq)
                 
             self.socketUDPack.sendto(seq, self.to)
                 
-                
+class Logger(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.time = 0
+        
+
+    def run(self):
+        global err,cnt,cLock,START
+        START.wait()
+        while True:
+          time.sleep(1)
+          cLock.acquire()
+          self.time += 1
+          print "\n",cnt,err,"\n" 
+          writer.writerow([self.time,cnt,err])
+          cnt = 0
+          err = 0
+          cLock.release()
+                   
+         
 if __name__ == '__main__':
     #port = 30000
     #while True:
@@ -58,6 +94,12 @@ if __name__ == '__main__':
     #        break
     #    except:
     #        pass
+    lfile = Logger()
+    lfile.setDaemon(True)
     UDP = UDPreceiver(PORT)
+    UDP.setDaemon(True)
     proxy.join(IP, PORT)
-    UDP.run()
+    UDP.start()
+    lfile.start()
+    while True:
+      time.sleep(100)
