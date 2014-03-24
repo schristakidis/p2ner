132f2bc480e6f7a78e30da78518054ae37f24a49
schristakidis <schristakidis@ece.upatras.gr>
2014-03-24 17:22:43 +0200

    log erros are printed in stderr

--- p2ner/components/interface/localcontrol/localcontrol/localcontrol.py
+++ p2ner/components/interface/localcontrol/localcontrol/localcontrol.py
@@ -19,62 +19,63 @@ from p2ner.core.components import getComponentsInterfaces as getCompInt
 from p2ner.base.Stream import Stream
 from p2ner.util.interfacelog import DatabaseLog
 from twisted.internet.threads import deferToThread
+import sys
 
 
 class localControl(Interface):
-    
+
     def initInterface(self):
         self.logger=DatabaseLog(_parent=self)
-        
+
     def start(self):
         pass
-        
-        
+
+
     def cleanUp(self):
         pass
-        
+
     def registerStream(self,stream,input=None,output=None):
         strm=Stream(**stream)
-        self.root.registerStream(strm,input,output)        
-    
+        self.root.registerStream(strm,input,output)
+
     def contactServers(self,server):
         for s in server:
             self.root.contactServers(s)
-    
+
     def startProducing(self,id,type):
         self.root.startProducing(id)
-    
+
     def startRemoteProducer(self,id,type):
         self.root.startRemoteProducer(id)
-        
-    
+
+
     def subscribeStream(self,id,ip,port,output=None):
         self.root.subscribeStream(id,ip,port,output)
 
-    
+
     def stopProducing(self,id,changeRepub=False):
         self.root.stopProducing(id,changeRepub)
-    
+
     def unregisterStream(self,id):
         self.root.unregisterStream(id)
-    
+
     def returnSubStream(self,stream,id):
         if stream==-1:
             self.log.info('could not subscribe stream with id %d',id)
         self.controlUI.succesfulSubscription(stream,id)
-        
+
 
     def returnStopProducing(self,id):
         self.controlUI.stopProducing(id)
-            
+
     def returnUnregisterStream(self,id):
         self.controlUI.unregisterStream(id)
-            
+
     def returnProducedStream(self,stream,hash=None):
         if stream==-1:
             self.log.info('could not register stream')
         self.controlUI.registerStream([stream])
-           
+
     def returnContents(self,stream,server):
         if stream==-1:
             message= 'server is unreachable: %s : %s '%(server[0],server[1])
@@ -83,40 +84,42 @@ class localControl(Interface):
 
     def setLiveProducer(self,id,live):
         self.controlUI.setLiveProducer(id,live)
-        
+
     def setLiveStream(self,id,live):
         self.controlUI.setLiveStream(id,live)
-           
+
     def removeProducer(self,id):
         self.controlUI.stopProducing(id)
-        
+
     def logRecord(self,record):
         if record.levelno==15:
             self.controlUI.newMessage(record.getMessage(),record.levelno)
             return
         elif record.name=='p2ner.network':
             self.controlUI.logNGui(record.getMessage())
+        if record.levelno==40:
+            sys.stderr.write(record.getMessage()+'\n')
         self.logger.addRecord(record)
-        
+
     def getLogRecords(self,func):
         d=self.logger.getRecords()
         d.addCallback(func)
-    
+
     def exiting(self):
         self.root.exiting()
-        
+
     def displayError(self,error,quit=False):
         self.controlUI.displayError(error,quit)
-    
-        
+
+
     def getComponentsInterfaces(self,comp):
         c=getCompInt(comp)
         self.controlUI.preferences.setComponent(comp,c)
-        
+
     def startConverting(self,gui,dir,filename,videorate,subs,subsFile,subsEnc):
         id=self.root.startConverting(dir,filename,videorate,subs,subsFile,subsEnc)
         gui.getConverterId(id)
-   
+
 
     def getConverterStatus(self,gui,id):
         status=self.root.converters[id].getStatus()
@@ -125,10 +128,10 @@ class localControl(Interface):
     def abortConverter(self,id):
         converter=self.root.converters.pop(id)
         converter.abort()
-        
+
     def getStatistics(self):
         return self.stats.keys()
-    
+
     def getStatValues(self,stats):
         ret=[]
         for s,c in stats:
@@ -136,15 +139,15 @@ class localControl(Interface):
             if count>100:
                 c=100 -( count - c)
             v=self.stats[s]['values'][c:]
-            
+
             ret.append((s,v,count))
         return ret
-    
+
     def networkUnreachable(self,status):
         reactor.callLater(0,self.displayError,'network conditions are not valid',True)
         self.networkStatus(status)
-        
-        
+
+
     def networkStatus(self,status):
         if status:
             self.controlUI.networkStatus(status,self.netChecker.externalIp,self.netChecker.measureBW)
@@ -153,31 +156,31 @@ class localControl(Interface):
             self.controlUI.networkStatus(status,None,None)
             msg='network condintions are not valid'
         reactor.callLater(0.2,self.log.log,15,msg)
-        
+
     def checkNetwork(self):
         reactor.callLater(0.2,self.log.log,15,'checking network conditions. Please wait...')
         reactor.callLater(0.3,self.netChecker.check)
         return
-        
+
     def newChatMessage(self,id,message,peer):
         self.controlUI.chatClientUI.newChatMessage(id,message,peer)
-        
+
     def newChatter(self,id,username,new):
         self.controlUI.chatClientUI.newChatter(id,username,new)
-        
+
     def sendChatMessage(self,id,message,peer):
         self.chatClient.sendChatMessage(id,message,peer)
-        
+
     def joinChatRoom(self,id,username,server):
         self.chatClient.joinChatRoom(id,username,server)
-    
+
     def leaveChatRoom(self,id,username,server):
         self.chatClient.leaveChatRoom(id,username,server)
 
-            
+
     def startBWMeasurement(self,ip,gui):
         self.root.startBWMeasurement(ip,gui)
-        
+
     def setBW(self,bw):
         self.root.setBW(bw)
- 
+
