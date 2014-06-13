f1560fcc56fb41e9cdf2706277ae82fe495693bc
schristakidis <schristakidis@ece.upatras.gr>
2014-06-14 00:38:14 +0300

    commit

--- p2ner/components/pipeelement/wrapperheaderparserelement/wrapperheaderparserelement/whparser.py
+++ p2ner/components/pipeelement/wrapperheaderparserelement/wrapperheaderparserelement/whparser.py
@@ -21,14 +21,12 @@ class WrapperHeaderParserElement(PipeElement):
 
     def initElement(self, *args, **kwargs):
         self.log.info('WrapperHeaderParser loaded')
-    
+
     def send(self, res, msg, data, peer):
         h=struct.pack('!?',False)
         h=''.join([h,res])
-        tPipe=self.trafficPipe.getElement(name="UDPPortElement")
-        tPipe.send(h,msg,data,peer)
-        self.breakCall()
-        
+        return h
+
     def receive(self,todecode,(host, port), recTime):
         d=self.forwardprev("receive", (host, port), recTime,dataPort=True)
         d.callback(todecode)
